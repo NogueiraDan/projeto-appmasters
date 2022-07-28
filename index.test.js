3777e4421c15ac653097bea809cff9eb3131689e
Daniel Nogueira <silvanogueira3460@gmail.com>
2022-07-28 11:45:24 -0300

    Implementando testes na rota POST

--- index.test.js
+++ index.test.js
@@ -1,13 +1,133 @@
 const app = require("./server");
 const request = require("supertest");
-describe("Testando server", () => {
-  it("Acesso a resposta da rota inicial", async () => {
+
+describe("Testando rotas da API", () => {
+  it("Deve acessar a resposta da rota inicial", async () => {
     const response = await request(app).get("/");
     expect(response.body.alive).toBeTruthy();
   });
 
-  it("Acesso ao código da resposta", async () => {
+  it("Deve acessar ao código da rota inicial", async () => {
     const response = await request(app).get("/");
+    expect(response.statusCode).toEqual(200);
+  });
+
+  it("Deve faltar um campo no corpo da requisição", async () => {
+    const response = await request(app)
+      .post("/donation")
+      .send({
+        email: "daniel@gmail.com",
+        phone: "(99) 99999-9999",
+        zip: "00000-000",
+        city: "City",
+        state: "PB",
+        streetAddress: "Rua das Tulipas",
+        number: "90",
+        complement: "Perto da Rua Flores",
+        neighborhood: "Ao redor da praça central",
+        deviceCount: 3,
+        devices: [
+          { type: "notebook", condition: "working" },
+          { type: "desktop", condition: "notWorking" },
+          { type: "netbook", condition: "notWorking" },
+        ],
+      });
+    expect(response.statusCode).toEqual(400);
+  });
+
+  it("Deve enviar email inválido", async () => {
+    const response = await request(app)
+      .post("/donation")
+      .send({
+        name: "Daniel",
+        email: "daniel",
+        phone: "(99) 99999-9999",
+        zip: "00000-000",
+        city: "City",
+        state: "PB",
+        streetAddress: "Rua das Tulipas",
+        number: "90",
+        complement: "Perto da Rua Flores",
+        neighborhood: "Ao redor da praça central",
+        deviceCount: 3,
+        devices: [
+          { type: "notebook", condition: "working" },
+          { type: "desktop", condition: "notWorking" },
+          { type: "netbook", condition: "notWorking" },
+        ],
+      });
+    expect(response.statusCode).toEqual(400);
+  });
+
+  it("Deve enviar quantidade de devices diferente dos declarados", async () => {
+    const response = await request(app)
+      .post("/donation")
+      .send({
+        name: "Daniel",
+        email: "daniel123@gmail.com",
+        phone: "(99) 99999-9999",
+        zip: "00000-000",
+        city: "City",
+        state: "PB",
+        streetAddress: "Rua das Tulipas",
+        number: "90",
+        complement: "Perto da Rua Flores",
+        neighborhood: "Ao redor da praça central",
+        deviceCount: 0,
+        devices: [
+          { type: "notebook", condition: "working" },
+          { type: "desktop", condition: "notWorking" },
+          { type: "netbook", condition: "notWorking" },
+        ],
+      });
+    expect(response.statusCode).toEqual(400);
+  });
+
+  it("Deve enviar dispositivo com type incorreto", async () => {
+    const response = await request(app)
+      .post("/donation")
+      .send({
+        name: "Daniel",
+        email: "daniel",
+        phone: "(99) 99999-9999",
+        zip: "00000-000",
+        city: "City",
+        state: "PB",
+        streetAddress: "Rua das Tulipas",
+        number: "90",
+        complement: "Perto da Rua Flores",
+        neighborhood: "Ao redor da praça central",
+        deviceCount: 3,
+        devices: [
+          { type: "typeIncorreto", condition: "working" },
+          { type: "desktop", condition: "notWorking" },
+          { type: "netbook", condition: "notWorking" },
+        ],
+      });
+    expect(response.statusCode).toEqual(400);
+  });
+
+  it("Deve enviar todos os dados corretos", async () => {
+    const response = await request(app)
+      .post("/donation")
+      .send({
+        name: "Daniel",
+        email: "daniel123@gmail.com",
+        phone: "(99) 99999-9999",
+        zip: "00000-000",
+        city: "City",
+        state: "PB",
+        streetAddress: "Rua das Tulipas",
+        number: "90",
+        complement: "Perto da Rua Flores",
+        neighborhood: "Ao redor da praça central",
+        deviceCount: 3,
+        devices: [
+          { type: "notebook", condition: "working" },
+          { type: "desktop", condition: "notWorking" },
+          { type: "netbook", condition: "notWorking" },
+        ],
+      });
     expect(response.statusCode).toEqual(200);
   });
 });
